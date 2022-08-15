44c0f94f2f772bc5726954dbd5d88262075d94d8
Nima Payandeh <nimapayandeh11@gmail.com>
2022-08-15 15:52:29 +0430

    First Commit

---
+++ android/app/src/main/kotlin/com/example/hive_todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hive_todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF4859F2);
+const darkBlue = Color(0xFF0A0C24);
+const backgroundColor = Color(0xFFFAFAFA);
+const whiteColor = Colors.white;
+const darkGreyColor = Color(0xFF909090);
+const greyColor = Color(0xFFECECEC);
+const lightGrey = Color(0xFFF6F6F6);
